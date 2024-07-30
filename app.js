9772e9b033f522f7b0ad99b0d426d8d54ffc10b7
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2024-07-30 19:23:03 +0530

    Added cors package and allowed from all

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ import connectDb from "./config/db.js";
 import urlsRouter from "./routes/urls.js";
 import indexRouter from "./routes/index.js";
 dotenv.config({ path: "./.env" });
+import cors from "cors";
 
 const app = express();
 
 // Body Parser
 app.use(urlencoded({ extended: true }));
 app.use(json());
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api", urlsRouter);
