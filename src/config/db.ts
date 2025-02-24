ea4048a9b2399fcb0416f34fd3087eaf77654ce4
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2025-02-24 21:44:15 +0530

    Added a check for mongo_uri in env

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,9 @@ import { connect } from "mongoose";
 
 const connectDb = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is available in env");
+    }
     await connect(process.env.MONGO_URI);
     console.log("DB Connected!");
   } catch (error) {
